c8e0352edeec0e603c1e17f781af2075759d66db
Kyle Buzsaki <kbuzsaki@gmail.com>
2022-01-23 01:13:01 -0800

    Add WIP replay fetcher and replay analysis button. No UI yet.

---
+++ content_scripts/replay_analysis_script.js
@@ -0,0 +1,45 @@
+(function() {
+    const kSidebarButtonHtml = `
+    <section>
+        <div class="game-tools-btn" style="margin-left: 5px;">
+            <div class="game-tools-bg">
+                <img src="terrain/userstats.gif" style="">
+            </div>
+            <span class="game-tools-btn-text small_text">Replay Analysis</span>
+        </div>
+    </section>
+    `;
+    function makeChartButton() {
+        let tempNode = document.createElement("div");
+        tempNode.innerHTML = kSidebarButtonHtml;
+        return tempNode.children[0];
+    }
+
+    function getGameId() {
+        var urlParams = new URLSearchParams(window.location.search);
+        if (urlParams.has("games_id")) {
+            return parseInt(urlParams.get("games_id"));
+        } else if (urlParams.has("replays_id")) {
+            return parseInt(urlParams.get("replays_id"));
+        }
+        return undefined;
+    }
+
+    // TODO: should this hide when the replay is closed?
+    let replayControls = document.querySelector("section.replay-controls");
+    let chartButton = makeChartButton();
+    replayControls.parentNode.insertBefore(chartButton, replayControls.nextSibling);
+
+    let gameId = getGameId();
+
+    let replayFetcher = new ReplayFetcher();
+    chartButton.addEventListener("click", (event) => {
+        console.log("Clicked analysis button");
+        /*
+        replayFetcher.fetchReplay(gameId).then((result) => {
+            console.log("Fetched replay state:", result);
+        });
+        */
+    });
+
+})();
